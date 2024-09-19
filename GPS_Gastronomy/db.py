3868dfcfc7e594dfa0ecfc1a3a12be17530d3953
xKendovVul <hadyihernandez@gmail.com>
2024-09-19 15:24:12 -0600

    primer commit

---
+++ GPS_Gastronomy/db.py
@@ -0,0 +1,10 @@
+postgresql = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Minero64',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
