383bc6d6b7c7c7da0987edaa02b57ac9af25bd81
bryan_alvarado <bryanalvarado1904@gmail.com>
2024-09-19 21:32:10 -0600

    create models user

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=40)
+    lastname = models.CharField(max_length=40)
+    age = models.IntegerField(max_length=3)
+    gender = models.CharField(max_length=40)
+    email = models.EmailField(max_length=40)
+    cellphone = models.IntegerField(max_length=10)
+    procedence = models.CharField(max_length=40)
+    password =models.FloatField(max_length=50)
+    
+    
