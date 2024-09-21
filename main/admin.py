6ce3f8961d234ab42ad7130975ed62daad1e8057
xKendovVul <hadyihernandez@gmail.com>
2024-09-21 16:01:41 -0600

    rama mia

--- main/admin.py
+++ main/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from main import models
 
 # Register your models here.
+
+admin.site.register(models.User)
--- main/models.py
+++ main/models.py
@@ -5,10 +5,10 @@ from django.contrib.auth.models import User
 class User(models.Model):
     name = models.CharField(max_length=40)
     lastname = models.CharField(max_length=40)
-    age = models.IntegerField(max_digits=3)
+    age = models.IntegerField()
     gender = models.CharField(max_length=40, choices=[('M', 'Masculino'), ('F', 'Femenino')])
     email = models.EmailField(max_length=40)
-    cellphone = models.IntegerField(max_digits=10)
+    cellphone = models.IntegerField()
     procedence = models.CharField(max_length=40)
     password =models.FloatField(max_length=50)
     
