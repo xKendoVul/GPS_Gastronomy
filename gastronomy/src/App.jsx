9272d56dcbef85d5dfcda7bf6f10cb65a52cdbe0
xKendovVul <hadyihernandez@gmail.com>
2024-09-21 17:14:01 -0600

    rama mia mas cambios paisa

--- gastronomy/src/App.jsx
+++ gastronomy/src/App.jsx
@@ -1,11 +1,11 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import {Home} from './pages/Home';
+import {HomePage} from './pages/HomPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />      
+        <Route path="/" element={<HomePage />} />      
       </Routes>
     </BrowserRouter>
   );
