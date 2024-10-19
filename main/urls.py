f827f80e42ef9917f19f83e248d02add3282edf1
xKendovVul <hadyihernandez@gmail.com>
2024-10-18 20:49:42 -0600

    autenticacion backend aun no funcional

--- main/urls.py
+++ main/urls.py
@@ -2,11 +2,16 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework.documentation import include_docs_urls
 from main import views
+from django.urls import path, include, re_path
+from .views import login, register, profile
 
 router = routers.DefaultRouter()
 router.register(r'food', views.NicaFoodView, 'food')
 
 urlpatterns = [
     path('food/', include(router.urls)),
+    re_path('login/', login),
+    re_path('register/', register),
+    re_path('profile/', profile),
     path('docs/', include_docs_urls(title='GPS Gastronomy API')),
 ]
